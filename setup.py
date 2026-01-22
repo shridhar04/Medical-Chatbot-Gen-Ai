31fd2af8b1241dba7bbb72bdfdcc4c98ddbc5b61
Shridhar <shridharpatil461997@gmail.com>
2026-01-22 22:39:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import findpackages , setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author = "Boktar Ahmed Bappy",
+    author_email = "shridharpatil461997@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
